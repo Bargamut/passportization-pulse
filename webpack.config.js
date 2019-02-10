0925b4428d6cff3f4d6fee3ad46f108557d6076c
Paul Petrov <paul@bargamut.ru>
2019-02-10 23:13:02 +0300

    Создать базовые точки входа, настроить конфиги

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,53 @@
 let path = require('path');
 
-let conf = {};
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+	isDevMode = process.env.NODE_ENV !== 'production';
 
-module.exports = conf;
+let conf = {
+	// Точка(-и) входа
+	entry: [
+		'./src/index.js',
+		'./src/mainpage.css',
+		'./src/page.css'
+	],
+	// Точка выхода
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'main.js',
+		// Относительный адрес ссылки на файлы с ресурсами для dev-сервера
+		publicPath: 'dist/'
+	},
+	devServer: {
+		// Показывать ошибки компиляции в браузере
+		overlay: true
+	},
+	module: {
+		rules: [
+			// Подключение Babel загрузчика
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [{
+						loader: isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader
+					},
+					"css-loader"
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin()
+	]
+};
+
+module.exports = (env, options) => {
+	let isProduction = options.mode === 'production';
+
+	conf.devtool = isProduction ? false : 'eval-sourcemap';
+
+	return conf;
+};
