851f1fcac33aeb5c18f98daf0208bcf34d5f6083
Paul Petrov <paul@bargamut.ru>
2020-08-06 03:09:46 +0300

    Split JS to extended files

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import './style.css';
+import {
+  fetchHelpers,
+  renderHumans,
+  renderEvents,
+} from './scripts/helpers';
 
 (async () => {
-	const docFragment = document.createDocumentFragment();
-	const helpersListElement = document.querySelector(`.project-helpers-list`);
+  const dataHelpersListElement = document.querySelector(`.data-helpers`);
+	const infoHelpersListElement = document.querySelector(`.info-helpers`);
 
-	const fetchHelpers = await fetch(`/data-helpers.json`);
-	const helpersData = await fetchHelpers.json();
+  dataHelpersListElement.append(
+    renderHumans(
+      await fetchHelpers(`/data-helpers.json`)
+    )
+  );
 
-	helpersData.forEach((item) => {
-    if (!item.isActive) return;
-
-    const fullName = item.nickName
-      ? `${item.firstName} "${item.nickName}" ${item.lastName}`
-      : `${item.firstName} ${item.lastName}`;
-
-    const itemElement = document.createElement(`li`);
-
-    itemElement.classList.add(`project-helpers-list__item`);
-
-		itemElement.innerHTML = `
-				<a class="project-helpers-list__link" href="${item.socials.vk}">
-					<img class="project-helpers-list__image" src="${item.avatar.image}">
-				</a>
-				${fullName}`;
-
-		docFragment.append(itemElement);
-	});
-
-	helpersListElement.append(docFragment);
+  infoHelpersListElement.append(
+    renderEvents(
+      await fetchHelpers(`/data-helpers__info.json`)
+    )
+  );
 })();
