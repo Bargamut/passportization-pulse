b66f6d4ed4f68ca0bb9a78eb2da1863345918004
Paul Petrov <paul@bargamut.ru>
2020-08-19 17:25:26 +0300

    Create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Header = () => (
+  <header className="site-header">
+    <div className="pulse-beat"></div>
+
+    <h1 className="site-header__title">Пульс паспортизации</h1>
+
+    <p>1 августа - 1 сентября 2020</p>
+  </header>
+);
+
+export default Header;
