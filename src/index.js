ea590660e27182eed7a4b9498ca1b72529fce7d4
Paul Petrov <paul@bargamut.ru>
2020-08-26 16:01:22 +0300

    Remove obsolete scripts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 import './style.css';
-import { share2Socials } from "./scripts/socials";
 
 ReactDOM.render(
   <App />,
